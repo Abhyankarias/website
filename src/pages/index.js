1dae19a72fa9248bad86349ee47a6495ed64d907
Koushik Debnath <debnathkoushik605@gmail.com>
2025-06-08 21:07:59 +0530

    feat: adds about us section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/HeroSection';
 import FounderSection from '@/components/FounderSection';
+import AboutUs from '@/components/AboutUs';
 
 export default function Home() {
   return (
     <div className='container mx-auto pb-8 px-4'>
       <HeroSection className='mt-10' />
       <FounderSection className='mt-20' />
+      <AboutUs className='mt-20' />
     </div>
   );
 }
