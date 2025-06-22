d6454fb24be4def70b4b3e303c46b1eb3a5bd0ad
Chimmoy Debnath <chinmoy.debnath11@gmail.com>
2025-06-22 13:39:33 +0530

    fix: nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-import { PHASE_PRODUCTION_SERVER } from 'next/constants.js';
 
-const isProduction = PHASE_PRODUCTION_SERVER === 'production';
-
-//const nextConfig = {
-//  reactStrictMode: true,
-//  assetPrefix: PHASE_PRODUCTION_SERVER ? '/abhyankarias.github.io/website' : undefined,
-//};
-
-
-const nextConfig = (phase) => {
-
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const config = {
-    reactStrictMode: true,
-  }
-  return config
-}
+const nextConfig = {
+  reactStrictMode: true,
+  //assetPrefix: PHASE_PRODUCTION_SERVER ? '/abhyankarias.github.io/website' : undefined,
+};
 
 export default nextConfig;
