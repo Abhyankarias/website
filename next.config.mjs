f23c445e637ee4e9adce50f563926d7fe0b30251
Chimmoy Debnath <chinmoy.debnath11@gmail.com>
2025-06-22 13:35:07 +0530

    fix: background image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // basePath: '/abhyankarias.github.io/website',
-};
+
+import { PHASE_PRODUCTION_SERVER } from 'next/constants.js';
+
+const isProduction = PHASE_PRODUCTION_SERVER === 'production';
+
+//const nextConfig = {
+//  reactStrictMode: true,
+//  assetPrefix: PHASE_PRODUCTION_SERVER ? '/abhyankarias.github.io/website' : undefined,
+//};
+
+
+const nextConfig = (phase) => {
+
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const config = {
+    reactStrictMode: true,
+  }
+  return config
+}
 
 export default nextConfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,28 +10,48 @@ import '@/styles/globals.css';
 
 import Head from 'next/head';
 import Layout from '@/components/layout';
+import { getImageProps } from 'next/image'
+
+function getBackgroundImage(srcSet = '') {
+  const imageSet = srcSet
+    .split(', ')
+    .map((str) => {
+      const [url, dpi] = str.split(' ')
+      return `url("${url}") ${dpi}`
+    })
+    .join(', ')
+  return `image-set(${imageSet})`
+}
+
+/*
+
+  className={`${libreBaskervilleRegular.variable} ${libreBaskervilleItalic.variable} ${libreBaskervilleBold.variable} ${leagueSpartanBold.variable} ${leagueSpartanRegular.variable} ${leagueSpartanMedium.variable} main-body`}
+  style={style}
+*/
 
 export default function MyApp({ Component, pageProps }) {
+  const {
+    props: { srcSet },
+  } = getImageProps({ alt: '', width: 128, height: 128, src: '/images/backgroundImage.jpg' })
+  const backgroundImage = getBackgroundImage(srcSet)
+  const style = { height: '100vh', width: '100vw', backgroundImage }
+
   return (
     <>
       <Head>
         <title>Abhyankar’s IAS</title>
-        {/* <link
+        {/* 
+        className={`${leagueSpartanBold.variable} ${leagueSpartanMedium.variable} ${leagueSpartanRegular.variable}`}
+        <link
           rel='preload'
           href='/images/BackgroundImage.jpg'
           as='image'
           media='(min-width: 48rem)'
         /> */}
       </Head>
-      <Layout
-        className={`${leagueSpartanBold.variable} ${leagueSpartanMedium.variable} ${leagueSpartanRegular.variable}`}
-      >
-        <main
-          className={`${libreBaskervilleRegular.variable} ${libreBaskervilleItalic.variable} ${libreBaskervilleBold.variable} ${leagueSpartanBold.variable} ${leagueSpartanRegular.variable} ${leagueSpartanMedium.variable} main-body`}
-        >
-          {/* Apply the font class here */}
-          <Component {...pageProps} />
-        </main>
+      <Layout >
+        {/* Apply the font class here */}
+        <Component {...pageProps} />
       </Layout>
     </>
   );
