03ba7c8a62e52c9e2fe9069c4fae4e9f71dca49a
Koushik Debnath <debnathkoushik605@gmail.com>
2025-07-31 08:07:46 +0530

    fix(build): reverts back to config fof a passed build

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,7 @@
-import { PHASE_PRODUCTION_SERVER } from 'next/constants.js';
+/** @type {import('next').NextConfig} */
 
-const nextConfig = (phase, { defaultConfig }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const isProductionServer = phase === PHASE_PRODUCTION_SERVER;
-  const basePath = isProductionServer ? '/website' : '';
-  const returnObject = {
-    reactStrictMode: isProductionServer ? false : true,
-    basePath,
-    assetPrefix: isProductionServer ? 'https://abhyankarias.github.io' : '',
-    // images: {
-    //   dangerouslyAllowSVG: true,
-    //   contentDispositionType: 'attachment',
-    //   contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    // },
-  };
-
-  return { ...(defaultConfig || {}), ...returnObject };
+const nextConfig = {
+  reactStrictMode: true,
+  //assetPrefix: PHASE_PRODUCTION_SERVER ? '/abhyankarias.github.io/website' : undefined,
 };
-
 export default nextConfig;
