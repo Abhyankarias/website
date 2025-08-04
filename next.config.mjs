aec63b0882db4b295563d7d5dc9d22c597d63bc1
Koushik Debnath <debnathkoushik605@gmail.com>
2025-08-04 12:29:32 +0530

    fix(build): updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,7 @@
-// import { PHASE_PRODUCTION_SERVER } from 'next/constants.js';
+/** @type {import('next').NextConfig} */
 
-const nextConfig = () => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  // const isProductionServer = phase === PHASE_PRODUCTION_SERVER;
-  // const basePath = isProductionServer ? '/website' : '';
-  const returnObject = {
-    reactStrictMode: true,
-    // basePath,
-    // assetPrefix: isProductionServer
-    //   ? 'https://abhyankarias.github.io'
-    //   : undefined,
-    // images: {
-    //   dangerouslyAllowSVG: true,
-    //   contentDispositionType: 'attachment',
-    //   contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    // },
-  };
-
-  return { ...returnObject };
+const nextConfig = {
+  reactStrictMode: true,
 };
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
