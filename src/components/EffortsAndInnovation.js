301bfe31cd2eaf8a161b7b0a57d8f2ec984ffc5b
Koushik Debnath <debnathkoushik605@gmail.com>
2025-08-29 12:31:06 +0530

    feat(efforts & innovation): adds efforts & innovation section

---
+++ src/components/EffortsAndInnovation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Image from 'next/image';
+import cx from 'classnames';
+
+const TITLE = 'Our Efforts & Innovation';
+const EFFORTS_AND_INNOVATION = [
+  'Experienced teachers with good academic pedagogic skills.',
+  'Tutors for feedback and recapitulation sessions.',
+  'Foundational concept-based teaching/ pedagogy based upon NCERT and University recommended texts and journals (additional subject/ issues specific reading/ reference and updating) in the journals in the respective fields and domains.',
+  'More of interaction (interactive classroom sessions) would be encouraged along with conceptual understanding and analytical depth.',
+];
+
+export default function EffortsAndInnovation({ className }) {
+  return (
+    <section
+      className={cx(
+        className,
+        'flex flex-col gap-4 p-4 bg-[#FFFFFF] rounded-md'
+      )}
+    >
+      <h2 className='title ml-9'>{TITLE}</h2>
+      <ul className='flex flex-col gap-4'>
+        {EFFORTS_AND_INNOVATION.map((list, index) => (
+          <li className='flex gap-4'>
+            <Image
+              src='website/images/point.svg'
+              alt={`Point number ${index + 1}`}
+              width={18}
+              height={18}
+            />
+            <p key={index} className='font-regular'>
+              {list}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import FounderSection from '@/components/FounderSection';
 import AboutUs from '@/components/AboutUs';
 import Testimonials from '@/components/Testimonials';
 import Courses from '@/components/Courses';
+import EffortsAndInnovation from '@/components/EffortsAndInnovation';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <AboutUs className='mt-20' />
       <Courses className='mt-20' />
       <Testimonials className='mt-20' />
+      <EffortsAndInnovation className='mt-20' />
     </div>
   );
 }
